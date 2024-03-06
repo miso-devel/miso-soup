a18dbb3716ccd852c9739134cd16f1a2104c5060
miso-devel <kengo279@icloud.com>
2024-03-06 23:34:08 +0900

    実装変更

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import { MarkdownKey, MarkdownPair, MarkdownType } from "./types.d.ts";
+
+export const MARKDOWN_PAIR: MarkdownPair = {
+  "#": "h1",
+  "##": "h2",
+  "###": "h3",
+  "####": "h4",
+  "#####": "h5",
+  "######": "h6",
+  "": "p",
+};
+
+export const MARKDOWN_KEYS = Object.keys(MARKDOWN_PAIR) as MarkdownKey[];
+export const MARKDOWN_VALUES: MarkdownType[] = Object.values(MARKDOWN_PAIR);
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,24 @@
-import { Element, Token } from "./types.d.ts";
+import { MARKDOWN_KEYS, MARKDOWN_PAIR } from "./constants.ts";
+import { MarkdownType } from "./types.d.ts";
+import { Element, MarkdownKey } from "./types.d.ts";
 
 const mdSplitter = (markdown: string): string[] =>
   markdown.split(/\r\n|\r|\n/).filter((f) => f !== "");
 
-const getToken = (line: string): Token => {
-  const [elmType, ...value] = line.split(" ");
-  if (value.length === 0) return { head: "", value: elmType };
-  return { head: elmType, value: value.join(" ") };
-};
-
-const detectElementType = (token: Token): string => {
-  switch (token.head) {
-    case "#":
-      return "h1";
-    case "##":
-      return "h2";
-    case "###":
-      return "h3";
-    case "####":
-      return "h4";
-    case "#####":
-      return "h5";
-    case "######":
-      return "h6";
-    default:
-      return "p";
+const lineParser = (line: string): Element | Error => {
+  const [type, ...rest] = line.split(" ");
+  const value = rest.join(" ");
+  if (!MARKDOWN_KEYS.includes(type as MarkdownKey)) {
+    return { type: "p", value: line };
   }
-};
-
-const detectElement = (line: string): Element => {
-  const token = getToken(line);
-  const type = detectElementType(token);
-  return { type, value: token.value };
+  return { type: MARKDOWN_PAIR[type as MarkdownKey], value };
 };
 
 export const m2h = (markdown: string): string => {
   const lineArray = mdSplitter(markdown);
   lineArray.map((line) => {
-    const element = detectElement(line);
-    console.log("element: ", element);
+    const res = lineParser(line);
+    console.log("element: ", res);
   });
   return markdown;
 };
