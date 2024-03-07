75ebea33646e4f94b3cebbdbd98db75eaf678171
miso-devel <kengo279@icloud.com>
2024-03-07 22:49:34 +0900

    element to html util

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { MARKDOWN_KEYS, MARKDOWN_PAIR } from "./constants.ts";
-import { MarkdownType } from "./types.d.ts";
 import { Element, MarkdownKey } from "./types.d.ts";
 
 const mdSplitter = (markdown: string): string[] =>
   markdown.split(/\r\n|\r|\n/).filter((f) => f !== "");
 
-const lineParser = (line: string): Element | Error => {
+const lineParser = (line: string): Element => {
   const [type, ...rest] = line.split(" ");
   const value = rest.join(" ");
   if (!MARKDOWN_KEYS.includes(type as MarkdownKey)) {
@@ -14,11 +13,14 @@ const lineParser = (line: string): Element | Error => {
   return { type: MARKDOWN_PAIR[type as MarkdownKey], value };
 };
 
+const lineToHtml = (line: Element): string => {
+  const { type, value } = line;
+  return `<${type}>${value}</${type}>`;
+};
+
 export const m2h = (markdown: string): string => {
   const lineArray = mdSplitter(markdown);
-  lineArray.map((line) => {
-    const res = lineParser(line);
-    console.log("element: ", res);
-  });
-  return markdown;
+  const parsedLine = lineArray.map((line) => lineParser(line));
+  const html = parsedLine.map((line) => lineToHtml(line)).join("\n");
+  return html;
 };
